478d3bcf45fd88711de86c10a11803ee01d83c53
Håvard Gulldahl <havard.gulldahl@nrk.no>
2017-06-15 01:01:51 +0200

    Treat flask globals properly

--- src/funkyquizbot/app.py
+++ src/funkyquizbot/app.py
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 from envparse import env, ConfigurationError # pip install envparse
 env.read_envfile()
 
-from flask import Flask, request
-from flask_apscheduler import APScheduler # pip install Flask-APScheduler
+from flask import Flask, request, g, current_app
+#from flask_apscheduler import APScheduler # pip install Flask-APScheduler
 
 import fbmq
 from fbmq import Attachment, Template, QuickReply
@@ -25,22 +25,23 @@ SECRET_URI = '/{}'.format(env('SECRET_URI'))
 PAGE_ACCESS_TOKEN = env('PAGE_ACCESS_TOKEN')
 
 app = Flask(__name__)
-if app.debug is not True:   
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('python.log', maxBytes=1024 * 1024 * 100, backupCount=20)
-    file_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
-    app.logger.addHandler(file_handler)
+from logging.handlers import RotatingFileHandler
+file_handler = RotatingFileHandler('python.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+file_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+app.logger.addHandler(file_handler)
 
 page = fbmq.Page(PAGE_ACCESS_TOKEN)
 
-quizes = quizprizes = giphys = []
+with app.app_context():
+    # within this block, current_app points to app.
+    g.quizes = g.quizprizes = g.giphys = []
 
 @app.route(SECRET_URI, methods=['GET'])
 def handle_verification():
     'Get a GET request and try to verify it'
-    logger.debug('About to read a challenge')
+    app.logger.debug('About to read a challenge')
     token = request.args.get('hub.verify_token')
     if request.args.get('hub.mode', '') == 'subscribe' and \
         token is not None and token == SECRET_CHALLENGE:
@@ -51,7 +52,7 @@ def handle_verification():
 @app.route(SECRET_URI, methods=['POST'])
 def handle_message():
     'Get a POST request and treat it like an incoming message'
-    logger.debug('Incoming payload')
+    app.logger.debug('Incoming payload')
     postdata = request.get_data(as_text=True)
 
     page.handle_webhook(postdata) # fbmq distributes according to @decorators
@@ -60,11 +61,11 @@ def handle_message():
 @page.after_send
 def after_send(payload, response):
     """:type event: fbmq.Payload"""
-    logger.debug("complete")
+    app.logger.debug("complete")
 
 def receipt(payload, response):
     "a callback that receives a message receipt"
-    logger.debug('response : ' + response.text)
+    app.logger.debug('response : ' + response.text)
 
 def encode_payload(prefix, data):
     """Return a <data> as a string, prefixed with <prefix>, for use as callback payload.
@@ -87,16 +88,16 @@ def message_handler(event):
     """:type event: fbmq.Event"""
     sender_id = event.sender_id
     message = event.message_text
-    logger.debug('New msg from %s: %r', sender_id, message)
+    app.logger.debug('New msg from %s: %r', sender_id, message)
     page.typing_on(sender_id)
     if message is None:
-        logger.debug("message is none, is this a thumbs up?")
+        app.logger.debug("message is none, is this a thumbs up?")
     elif message.lower() in ['quiz',]:
         quiz(event)
     elif event.is_postback:
-        logger.debug("this is postback, someone else must handle it")
+        app.logger.debug("this is postback, someone else must handle it")
     elif event.is_quick_reply:
-        logger.debug("this is quickreply, someone else must handle it")
+        app.logger.debug("this is quickreply, someone else must handle it")
     else:
         page.send(sender_id, "thank you, '%s' yourself! type 'quiz' to start it :)" % message, callback=receipt)
 
@@ -119,10 +120,9 @@ def quiz(event, previous=None):
             return
     # ask a question
     try:
-        global quizes
-        quiz = random.choice(quizes) # get a random quiz
+        quiz = random.choice(g.quizes) # get a random quiz
         while quiz.qid in previous:
-            quiz = random.choice(quizes) # we've had this ques before, get a new onone
+            quiz = random.choice(g.quizes) # we've had this ques before, get a new onone
     except IndexError:
         # no quizes in list, yikes
         page.send(sender_id, "We have no available quizes for you, pls try again later 8)")
@@ -138,7 +138,7 @@ def quiz(event, previous=None):
         QuickReply(title=quiz.correct, payload=encode_payload('ANSWER', {'previous':previous, 'correct':True})),
     )
     random.shuffle(buttons) # hide  correct answer
-    logger.debug("sending quiz: %s", quiz)
+    app.logger.debug("sending quiz: %s", quiz)
     page.send(sender_id, quiz.question, quick_replies=buttons)
     page.typing_off(sender_id)
 
@@ -150,10 +150,9 @@ def send_prize(event, previous=None):
     page.typing_on(sender_id)
     page.send(sender_id, "wow, you're on a nice streak. Here's a prize!")
     # Send a gif prize
-    global quizprizes
-    prize = random.choice(quizprizes)
+    prize = random.choice(g.quizprizes)
     while not prize.is_embargoed: # make sure we can publish this
-        prize = random.choice(quizprizes)
+        prize = random.choice(g.quizprizes)
     if prize.media_type == 'image':
         att = Attachment.Image(prize.url)
     elif prize.media_type == 'video':
@@ -162,8 +161,7 @@ def send_prize(event, previous=None):
 
 def get_giphy(context):
     "Get a random giphy that fits the context 'CORRECT'/'WRONG'"
-    global giphys
-    return random.choice([g for g in giphys if g.context == context])
+    return random.choice([x for x in g.giphys if x.context == context])
 
 @page.callback(['ANSWER_.+'])
 def callback_answer(payload, event):
@@ -171,7 +169,7 @@ def callback_answer(payload, event):
     sender_id = event.sender_id
     page.typing_on(sender_id)
     prefix, metadata = decode_payload(payload)
-    logger.debug('Got ANSWER: {} (correct? {})'.format(metadata, 'YES' if metadata['correct'] else 'NON'))
+    app.logger.debug('Got ANSWER: {} (correct? {})'.format(metadata, 'YES' if metadata['correct'] else 'NON'))
     page.send(sender_id, "Your reply was {}".format('CORRECT' if metadata['correct'] else 'INCORRECT :('))
     if random.random() > 0.9: # ten percent of the time, send a gif
         g = get_giphy('CORRECT' if metadata['correct'] else 'WRONG')
@@ -210,27 +208,32 @@ def getpickles(env_key):
     try:
         return pickle.load(open(env(env_key), 'rb'))
     except FileNotFoundError:
-        logger.warning('Could not load cached values from "{}"->{!r}'.format(env_key, env(env_key)))
+        app.logger.warning('Could not load cached values from "{}"->{!r}'.format(env_key, env(env_key)))
         return []
 
 def getquizdata():
     "Background task to periodically update quizes"
-    global quizes
-    logger.debug("Get new quizquestions, currently we have {!r}".format(quizes))
-    logger.debug("Get new quizquestions, from {!r}".format(env('CACHEFILE_QUIZQUESTIONS')))
-    quizes = getpickles('CACHEFILE_QUIZQUESTIONS')
+    app.logger.debug("Get new quizquestions, currently we have {!r}".format(g.get('quizes', None)))
+    app.logger.debug("Get new quizquestions, from {!r}".format(env('CACHEFILE_QUIZQUESTIONS')))
+    g.quizes = getpickles('CACHEFILE_QUIZQUESTIONS')
+    app.logger.debug("Read {} questions".format(len(g.get('quizes', []))))
 
 def getquizprizes():
     "Background task to periodically update quizesprizes"
-    global quizprizes
-    logger.debug("Get new quizprizes, currently we have {!r}".format(quizprizes))
-    quizprizes = getpickles('CACHEFILE_QUIZPRIZES')
+    app.logger.debug("Get new quizprizes, currently we have {!r}".format(g.get('quizprizes', None)))
+    g.quizprizes = getpickles('CACHEFILE_QUIZPRIZES')
 
 def getgiphys():
     "Background task to periodically update giphys"
-    global giphys
-    logger.debug("Get new giphys, currently we have {!r}".format(giphys))
-    giphys = getpickles('CACHEFILE_GIPHYS')
+    app.logger.debug("Get new giphys, currently we have {!r}".format(g.get('giphys', None)))
+    g.giphys = getpickles('CACHEFILE_GIPHYS')
+
+with app.app_context():
+    # within this block, current_app points to app.
+    print(current_app.name)
+    app.before_first_request(getquizdata)
+    app.before_first_request(getquizprizes)
+    app.before_first_request(getgiphys)
 
 class Config(object):
     JOBS = [
@@ -259,15 +262,15 @@ class Config(object):
     SCHEDULER_API_ENABLED = False # REST api to jobs
     SCHEDULER_TIMEZONE = 'Europe/Oslo'
 
-app.config.from_object(Config())
+#app.config.from_object(Config())
 # get quizes
-scheduler = APScheduler()
-scheduler.init_app(app)
-scheduler.start()
+#scheduler = APScheduler()
+#scheduler.init_app(app)
+#scheduler.start()
 
 if __name__ == '__main__':
     # start server
-    getquizdata()
-    getquizprizes()
-    getgiphys()
+    #getquizdata()
+    #getquizprizes()
+    #getgiphys()
     app.run(host='0.0.0.0', port=8000, debug=False, threaded=True)
